0263c2df0246737fc260dfb15f6e446a0c8f334e
komal8 <56218288+komal8@users.noreply.github.com>
2020-06-16 15:46:47 +0530

    Update Demo.java
    changes in master

--- Demo.java
+++ Demo.java
@@ -2,6 +2,8 @@ class Demo{
 public static void main(String args[]){
 System.out.println("This is first java project");
   System.out.println("This is modified file again");
+  System.out.println("modification in masterbranch");
+  System.out.println("Integration");
 }
 
 
